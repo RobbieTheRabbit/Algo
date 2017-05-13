06f544eb378c08787cf2d998c98d21f8ac8d2692
Sven <flynn23@gmx.de>
2017-05-13 18:16:51 +0200

    Änderung des Counters
    Beide Vairanten sind jetzt vorhanden

--- src/uebung1/MergeSort2.java
+++ src/uebung1/MergeSort2.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 import uebung1.FileIntArray;
 
 /*********************************************************************
- * Erste praktische Übung von Algorithmen SS17 - Merge Sort Verbunden mit
- * einlesen von Datein, die Werte vorgegeben haben
+ * Erste praktische Übung von Algorithmen SS17 - Merge Sort
+ * Verbunden mit einlesen von Datein, die Werte vorgegeben haben
  * 
  * @author Sven Böhrnsen
  * @author Oliver Tili
@@ -20,26 +20,29 @@ public class MergeSort {
 	public static int count = 0;
 
 	public static void main(String[] args) {
-//		 int[] zahlenfolge = { 5, 9, 1, 4, 6, 7, 3, 2, 8 }; // Selbst gewählte Zahlen
-		// int[] zahlenfolge2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // Selbst gewählte Zahlen
-		// int[] zahlenfolge = readArray("src/uebung1/beispiele/Rand500_1"); // Aus einer Datei ausgelesene Zahlen
+//		int[] zahlenfolge = { 5, 6, 1, 0, 2, 3, 4 };	// Selbst gewählte Zahlen
+//		int[] zahlenfolge = readArray("src/uebung1/beispiele/Rand500_1");		// Aus eigener Funktion ausgelesene Zahlen
+
 
 		System.out.println("			    --Merge Sort--");
 		System.out.println("***************************************************************************");
 
-		MergeSort mSort = new MergeSort();
 		
+
+		MergeSort mSort = new MergeSort();
 //		mSort.sort(zahlenfolge);
-		mSort.sort(FileIntArray.FileToIntArray("src/uebung1/beispiele/Rand1000_1"));
-//		mSort.sort(FileIntArray.FileToIntArray("src/uebung1/beispiele/Sort1000_1"));
+//		mSort.sort(zahlenfolge2);
+//		mSort.sort(FileIntArray.FileToIntArray("src/uebung1/beispiele/Rand100000_2"));
+		mSort.sort(FileIntArray.FileToIntArray("src/uebung1/beispiele/Sort100000_1"));
 
 	}
 
+
 	/*********************************************************************
-	 * Kunstruktor für die Mergesort Methode, der das unsortierte und sortierte
-	 * Array in der Konsole ausgibt
+	 * Kunstruktor für die Mergesort Methode, der das unsortierte 
+	 * und sortierte Array in der Konsole ausgibt
 	 * 
-	 * @param values - Das übergebene, unsortierte, Array
+	 * @param values - Das übergebene, unsortierte, Array 
 	 *********************************************************************/
 	private void sort(int[] values) {
 		this.count = 0;
@@ -51,25 +54,26 @@ public class MergeSort {
 		System.out.print("Eingabe:	");
 		for (int i = 0; i <= number - 1; i++) {
 			System.out.print(sortArray[i] + " ");
+			count ++;
 		}
 		System.out.println("");
 		mergeSort(0, number - 1);
-
+		
 		System.out.print("Sortiert:	");
 		for (int i = 0; i <= number - 1; i++) {
 			System.out.print(sortArray[i] + " ");
+			count ++;
 		}
-
 		long runningTime = new Date().getTime() - start;
 		System.out.println("");
-		System.out.println("Durchläufe:	" + count);
-		System.out.println("Benötigte Zeit: " + runningTime + " Millisekunden");
+		System.out.println("Durchläufe: :" + count);
+		System.out.println("Benötigte Zeit: "+ runningTime + " Millisekunden");
 		System.out.println("---------------------------------------------------------------------------");
 	}
 
 	/*********************************************************************
-	 * Funktion teilt das Array anhand der mitte in mehrere Teilarrays, bis
-	 * jedes Element einzeln steht und fügt sie dann wieder sortiert zusammen
+	 * Funktion teilt das Array anhand der mitte in mehrere Teilarrays, 
+	 * bis jedes Element einzeln steht und fügt sie dann wieder sortiert zusammen
 	 * 
 	 * @param low - der kleinste Index im Array
 	 * @param high - der größte Index im Array
@@ -80,12 +84,13 @@ public class MergeSort {
 			mergeSort(low, mid);
 			mergeSort(mid + 1, high);
 			merge(low, mid, high);
+			count ++;
 		}
 	}
 
 	/*********************************************************************
-	 * Funktion um die einzelnen Elemente der Arrays miteiner vergleicht und
-	 * wieder in einem sortierten Array zusammenzufügt
+	 * Funktion um die einzelnen Elemente der Arrays miteiner vergleicht 
+	 * und wieder in einem sortierten Array zusammenzufügt
 	 * 
 	 * @param low - der kleinste Index im Array
 	 * @param mid - die Mitte der Arrays
@@ -94,6 +99,7 @@ public class MergeSort {
 	private void merge(int low, int mid, int high) {
 		for (int i = low; i <= high; i++) {
 			helpArray[i] = sortArray[i];
+			count ++;
 		}
 		int i = low;
 		int j = mid + 1;
@@ -102,50 +108,49 @@ public class MergeSort {
 			if (helpArray[i] <= helpArray[j]) {
 				sortArray[k] = helpArray[i];
 				i++;
-				count++;
 			} else {
 				sortArray[k] = helpArray[j];
 				j++;
-				count++;
 			}
 			k++;
+			count ++;
 		}
 		while (i <= mid) {
 			sortArray[k] = helpArray[i];
 			k++;
 			i++;
-			count++;
+			count ++;
 		}
 	}
-
+	
 	/*********************************************************************
 	 * Funktion liest eine Datei mit Werten ein und speichert sie in einem
 	 * int-Array
-	 * 
 	 * @param file - Die einzulesene Datei mit den Werten für das Array
 	 * @return Die Elemente aus der Datei in einem int-Array
 	 *********************************************************************/
-	public static int[] readArray(String file) {
-		int anzahl = 0;
-		try {
+	public static int[] readArray(String file){
+   	 int counter = 0;
+   	 try {
 			Scanner scanLaenge = new Scanner(new File(file));
-			while (scanLaenge.hasNextLine()) {
-				anzahl++;
+			while(scanLaenge.hasNextLine()){
+				counter++;
 				scanLaenge.nextLine();
 			}
-			int[] elemente = new int[anzahl];
-
+			int[] elemente = new int[counter];
+			
 			Scanner scanContent = new Scanner(new File(file));
-			for (int i = 0; i < anzahl; i++) {
+			for (int i =0; i < counter; i++){
 				elemente[i] = scanContent.nextInt();
 			}
 			return elemente;
-
+					
+					
 		} catch (FileNotFoundException e) {
 			System.out.println("Die Datei konnte nicht gefunden werden!");
 			e.printStackTrace();
 		}
 		return null;
-
-	}
+   	 
+    }
 }
